be7509fbf6201f792d3e9c023dc6e82e7845e0e9
SMireault <le0636183@crc-lennox.qc.ca>
2024-11-20 09:59:44 -0500

    Login-view done, refactor names for the controller and the project

--- src/main/java/com/example/integrationprojectsdoop2/MovieTheatreApplication.java
+++ src/main/java/com/example/integrationprojectsdoop2/MovieTheatreApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MovieTheatreApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MovieTheatreApplication.class.getResource("Login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 450, 513);
+        stage.setTitle("Log in");
         stage.setScene(scene);
         stage.show();
     }
