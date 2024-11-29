08aade988559a68a3bf25b07ee4538ed2757c7b3
Jarvy <jarvy@physiotec.org>
2024-11-29 10:17:38 -0500

    Enable back button in view

--- src/main/java/com/example/integrationprojectsdoop2/Controllers/ReportViewController.java
+++ src/main/java/com/example/integrationprojectsdoop2/Controllers/ReportViewController.java
@@ -1,14 +1,26 @@
 package com.example.integrationprojectsdoop2.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ReportViewController {
 
     @FXML
     public Label headerName;
 
+    @FXML
+    public AnchorPane salesReportView;
+
     public void setHeaderName(String pText) {
         headerName.setText(pText);
+    }
+
+
+    public void onBackButtonClick() {
+        Stage stage = (Stage) salesReportView.getScene().getWindow();
+        stage.close();
     }
 }
