708eaaacb669efae8453d3c7ac91d82252d53692
Jarvy <jarvy@physiotec.org>
2024-11-30 17:24:42 -0500

    Bring backbutton forward in reportview, populate dropdown option

--- src/main/java/com/example/integrationprojectsdoop2/Controllers/ReportViewController.java
+++ src/main/java/com/example/integrationprojectsdoop2/Controllers/ReportViewController.java
@@ -1,8 +1,11 @@
 package com.example.integrationprojectsdoop2.Controllers;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,6 +16,16 @@ public class ReportViewController {
 
     @FXML
     public AnchorPane salesReportView;
+    @FXML
+    public ListView reportListView;
+
+    @FXML
+    public ComboBox reportComboBox;
+
+
+    public void initialize() {
+        reportComboBox.getItems().addAll("Alphabetical", "Sales");
+    }
 
     public void setHeaderName(String pText) {
         headerName.setText(pText);
